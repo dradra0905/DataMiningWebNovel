7921d187aea721055f6ba3404e4fffa5c19adad0
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-12-08 19:04:57 +0900

    crawling 코드 수정

--- kakaopageCrawling/dm.py
+++ kakaopageCrawling/dm.py
@@ -15,72 +15,56 @@ driver.get('https://page.kakao.com/menu/10011/screen/84?is_complete=false')
 time.sleep(0.3)
 toScroll = driver.find_element_by_xpath('/html/body')
 
-txt = ""
-      
-# def func(txt):
 work = 10000
 link = []
+
+# 웹소설의 page의 URL을 가져오는 부분
 for i in range(1, work+1):
-    # print(i)
     if(i % 10 == 1):
         toScroll.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.1)
-    
         
     pt = '//*[@id="__next"]/div/div[2]/div/div[2]/div[1]/div/div[4]/div/div/div/div[{}]/div/a'.format(i)
     ss = driver.find_element_by_xpath(pt)
     link.append(ss.get_attribute("href"))
-    print("link: %d" %(i))
-    # print("count %d : %s" %(i, ss.get_attribute("href")))
-    # t = "count {}\n{}\n".format(i,ss.text)
-    # txt+=t
     
-
-file = open("urls.txt","w", encoding='utf-8')
+    print("link: %d" %(i))
+    
+file = open("urls2.txt","w", encoding='utf-8')
 file.write('\n'.join(link))
 file.close()
 
-for i in range(0,work):
-    # print(link[i])
+# URL을 가지고 웹소설의 정보 가져오기
+file = open("urls.txt","r", encoding='utf-8')
+data = file.readlines()
+file.close()
+
+txt = ""
+      
+for i in range(0,10000):
     # 작품 선택 + 정보 탭 선택
-    path = link[i] + '?tab_type=about'
+    path = data[i] + '?tab_type=about'
     driver.get(path)
-    time.sleep(0.3)
+    time.sleep(0.03)
             
     try:
-        subject = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/a/div').text
-        # print("제목 : " + subject)
-        txt+="제목 : {} ".format(i+1) + subject
-        txt+="\n\n"
-    # # 작품의 정보 탭 선택
-    # inf = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div[1]/div/div/div[2]/a/div')
-    # inf.click()
-    # time.sleep(0.3)
-
-        # 작품의 줄거리 텍스트
-        summary = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div[2]/div[2]')
-        # print(summary.text)
-        txt+=summary.text
-        txt+="\n\n"
+        # 작품의 조회수
+        view = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/a/div/div[1]/div[2]/span').text
+        txt+=view
+        txt+="\n"
 
         # 작품의 키워드
         keyword = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]')
-        # print(keyword.text)
         txt+=keyword.text
-        # print()
     
     except:
         continue
     
     finally:
-        txt+="\n\n\n"
-        txt+="-------------------------------------------------------------------\n"
+        txt+="\n--------\n"
         print("content: %d" %(i+1))
         
 
-
-# func(txt)
-# print(txt)
 file = open("data.txt","w", encoding='utf-8')
 file.write(txt)
 file.close()
